8e418063d14d11d719038bcff7842b65cd2ba3a1
timelessover <shanpaodaxia@qq.com>
2018-07-31 22:52:13 +0800

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+var keys = {
+    "0": {0:' q ',1:' w ',2:' e ',3:' r ',4:' t ',5:' y ',6:' u ',7:' i ',8:' o ',9:' p ',length:10},
+    "1": {0:' a ',1:' s ',2:' d ',3:' f ',4:' g ',5:' h ',6:' j ',7:' k ',8:' l ',length:9},
+    "2": {0:' z ',1:' x ',2:' c ',3:' v ',4:' b ',5:' n ',6:' m ',length:7},
+    length:3
+}
+var hash = {
+    "q":'qq.com',
+    "w":'weibo.com',
+    "e":'ele.com',
+    "r":'renren',
+    "t":'tianya.com',
+    "y":'youtube.com',
+    "u":'uc.com',
+    "i":'iqiyi.com',
+    "o":'opera.com',
+    "p": undefined,
+    "a":'acfun.tv',
+    "s":'sohu.com',
+    "z":'zhihu.com'
+}
+     var div1 = document.createElement('div')
+     x.appendChild(div1)
+     index = 0
+    while(index<keys['length']{
+        x = document.createElement('div')
+        y.appendChild(x)
+        index = index+1
+    }
